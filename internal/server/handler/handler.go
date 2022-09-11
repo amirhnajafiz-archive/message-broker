26d69b59f9f36b2a1d0ea51c5478604517a61cf3
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-11 08:30:56 +0430

    add: handler

--- internal/server/handler/handler.go
+++ internal/server/handler/handler.go
@@ -1,1 +1,25 @@
 package handler
+
+import (
+	"log"
+	"net"
+
+	"github.com/highway-to-victory/udemy-broker/pkg/network"
+)
+
+type Handler struct {
+	Network network.Network
+}
+
+func NewHandler(conn net.Conn) Handler {
+	return Handler{
+		Network: network.NewNetwork(conn),
+	}
+}
+
+func (h *Handler) Handle() {
+	err := h.Network.Send([]byte("Hello world"))
+	if err != nil {
+		log.Println(err)
+	}
+}
