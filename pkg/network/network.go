ea61b18d6239636a50ef0dcd662009ff3da30d37
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-11 08:25:49 +0430

    add: network module

--- pkg/network/network.go
+++ pkg/network/network.go
@@ -1,1 +1,36 @@
 package network
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+type Network struct {
+	connection net.Conn
+}
+
+func NewNetwork(conn net.Conn) Network {
+	return Network{
+		connection: conn,
+	}
+}
+
+func (n *Network) Send(data []byte) error {
+	if _, err := n.connection.Write(data); err != nil {
+		return fmt.Errorf("failed to send data: %w", err)
+	}
+
+	return nil
+}
+
+func (n *Network) Get(buffer []byte) ([]byte, error) {
+	bytes, err := n.connection.Read(buffer)
+	if err != nil {
+		if err != io.EOF {
+			return nil, fmt.Errorf("failed to get data: %w", err)
+		}
+	}
+
+	return buffer[:bytes], nil
+}
