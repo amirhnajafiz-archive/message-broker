e5762f47233274d8474431777e03e06bc855788a
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-11 08:32:53 +0430

    add: server

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,28 @@
 package server
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/highway-to-victory/udemy-broker/internal/server/handler"
+)
+
+func Start(address string) error {
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to start server: %w", err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			return fmt.Errorf("failed to accept client: %w", err)
+		}
+
+		fmt.Println("accept client")
+
+		h := handler.NewHandler(conn)
+
+		go h.Handle()
+	}
+}
