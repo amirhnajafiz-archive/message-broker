fc8f7304fe340980f14ed831059e29b7c37afd39
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-11 08:29:15 +0430

    add: client initialized

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,1 +1,39 @@
 package client
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/highway-to-victory/udemy-broker/pkg/network"
+)
+
+type Client struct {
+	Network network.Network
+}
+
+func NewClient(address string) (*Client, error) {
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		return nil, fmt.Errorf("connection failed: %w", err)
+	}
+
+	var c Client
+
+	c.Network = network.NewNetwork(conn)
+
+	return &c, nil
+}
+
+func (c *Client) Start() {
+	var buffer = make([]byte, 2048)
+
+	for {
+		data, err := c.Network.Get(buffer)
+		if err != nil {
+			log.Println(err)
+		}
+
+		fmt.Println(string(data))
+	}
+}
