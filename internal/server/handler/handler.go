e6361c6dbfc41508650afb17844eb132c2859133
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-11 09:12:15 +0430

    update: handler

--- internal/server/handler/handler.go
+++ internal/server/handler/handler.go
@@ -8,18 +8,47 @@ import (
 )
 
 type Handler struct {
-	Network network.Network
+	Id          int
+	GetChannel  chan []byte
+	SendChannel chan []byte
+	Network     network.Network
 }
 
-func NewHandler(conn net.Conn) Handler {
+func NewHandler(conn net.Conn, channel chan []byte) Handler {
 	return Handler{
-		Network: network.NewNetwork(conn),
+		GetChannel:  channel,
+		SendChannel: make(chan []byte),
+		Network:     network.NewNetwork(conn),
 	}
 }
 
 func (h *Handler) Handle() {
-	err := h.Network.Send([]byte("Hello world"))
-	if err != nil {
-		log.Println(err)
+	go h.listenForDataToSend()
+	go h.listenForDataToSend()
+}
+
+func (h *Handler) listenForDataToSend() {
+	for {
+		message := <-h.GetChannel
+
+		err := h.Network.Send(message)
+		if err != nil {
+			log.Println(err)
+		}
+	}
+}
+
+func (h *Handler) listenForDataToGet() {
+	var buffer = make([]byte, 2048)
+
+	for {
+		data, err := h.Network.Get(buffer)
+		if err != nil {
+			log.Println(err)
+
+			continue
+		}
+
+		h.SendChannel <- data
 	}
 }
