53787a7e2f943414ddde350813bba72c1bb63564
amirhnajafiz <najafizadeh21@gmail.com>
2022-09-11 09:15:43 +0430

    add: broker service

--- internal/server/broker/broker.go
+++ internal/server/broker/broker.go
@@ -1,1 +1,32 @@
 package broker
+
+import "github.com/highway-to-victory/udemy-broker/internal/server/handler"
+
+type Broker struct {
+	MainChannel chan []byte
+	Handlers    []*handler.Handler
+}
+
+func NewBroker() Broker {
+	return Broker{
+		MainChannel: make(chan []byte),
+	}
+}
+
+func (b *Broker) Start() {
+	for {
+		data := <-b.MainChannel
+
+		go b.SendData(data)
+	}
+}
+
+func (b *Broker) AddWorker(h *handler.Handler) {
+	b.Handlers = append(b.Handlers, h)
+}
+
+func (b *Broker) SendData(data []byte) {
+	for _, worker := range b.Handlers {
+		worker.GetChannel <- data
+	}
+}
